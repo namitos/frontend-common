0a7fb905d2cbcd338195b6e67679bfbbd6f7192a
Artem <namitos@users.noreply.github.com>
2018-11-20 17:04:52 +0300

    Update BaseComponent.js

--- BaseComponent.js
+++ BaseComponent.js
@@ -1,6 +1,7 @@
 import { BaseComponentMixin } from './BaseComponentMixin.js';
 import { render, html } from '../lit-html/lit-html.js';
 
+export { html };
 export class BaseComponent extends BaseComponentMixin(HTMLElement) {
   template() {
     throw { name: 'BaseComponentError', text: 'template() method required' };
